e45dcd9ea5d3086bdb5a024ddd2404d534074768
Santiago Porras <sporrass@unbosque.edu.co>
2021-10-22 18:07:38 -0500

    Adding new backgrounds

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import React from "react";
-import { Text, View, StyleSheet, Image } from "react-native";
+import { Text, View, StyleSheet, Image,TouchableOpacity } from "react-native";
 import image from './assets/UEB-logo.png'
 
 const App = () => {
   return (
+
+  
     <View style={styles.container}>
-      <Text style={styles.title}> The Finder !</Text>
+
+      <Text style={styles.title}> THE FINDER </Text>
       <Image
         source={image}
         style={styles.image}
@@ -18,13 +21,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1, justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: "#EEAB02",
+    backgroundColor: "aliceblue",
+    padding:20,
     
   },
-  title: { fontSize: 50 },
+  title: { fontSize: 40, fontWeight:'bold',flex:1,padding:20},
+
   image: { height: 200, width: 220, alignItems:'center' }
 
-
-
 })
+
+
 export default App;
